3c2aee6b886732d4bce78bf94c1200c237dd4bbb
ac-he <ach28574@bethel.edu>
2022-03-23 13:40:01 -0500

    Cleanup of Main.py and Game.py
    - added more comments
    - added docstrings on all methods in game.py
    - added some type hints where I could
    - cleaned up some logic stuff where method calls and variable assignments had gotten repetitive or hard to follow
    - organized and rearranged methods in game.py based on what aspects they deal with

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
 import copy
-
 import move
 import pieces
 from piece_movement import is_enemy_in_checkmate
 
 
+# ----------- Board -----------
+
+
 def create_board():
+    """Creates a blank board and makes each individual tile
+    :returns created board"""
+
     board = []
     rows, cols = (10, 10)
     for i in range(rows):
@@ -17,10 +22,23 @@ def create_board():
     return board
 
 
+# create global board variable
 board = create_board()
 
 
-def get_opposite_color(string):
+def get_game_board():
+    """Getter method for the game board
+    :returns current state of the board"""
+    return board
+
+
+# ----------- Turns -----------
+
+
+def get_opposite_color(string: str) -> str:
+    """Finds the color opposite of the one provided
+    'white' -> 'black', 'black' -> 'white', 'anything else' -> ' '
+    """
     return_color = " "
     if string == "white":
         return_color = "black"
@@ -29,76 +47,93 @@ def get_opposite_color(string):
     return return_color
 
 
-def switch_turns():
+def switch_turns() -> str:
+    """Switches to the turn 'opposite' of the current turn
+    :returns the turn that was switched to"""
     global turn
     turn = get_opposite_color(turn)
     return turn
 
 
-def get_game_board():
-    # for r in Board:
-        # for f in r:
-            # print(f["coord"], end="")
-            # if f["tile class"] == "whiteSelected" or f["tile class"] == "blackSelected":
-            #    print("s", end="")
-            # print(f["piece"]["label"], end=" ")
-        # print()
-    return board
+turn = ""  # create global variable to track the current player/whose turn it is
 
 
-turn = ""
-is_over = False
-
-
-def create_turn():
+def create_turn() -> str:
+    """Sets up the initial turn"""
     global turn
     turn = "white"
     return turn
 
 
-def get_cur_turn():
+def get_cur_turn() -> str:
+    """Getter method to return the current turn"""
     global turn
     return turn
 
 
-def clear_click():
+def clear_click() -> dict:
+    """Returns a cleared tile with no class, coordinates, or location"""
     return {"tile class": "none", "coord": "none", "rank": -1, "file": -1}
 
 
-def get_is_over():
+# ----------- Game ending -----------
+
+
+is_over = False  # create global variable to track if the game is over
+
+
+def get_is_over() -> bool:
+    """Returns true if the game is over, false if game is in progress"""
     global is_over
     return is_over
 
 
-def set_is_over(over):
+def set_is_over(over: bool) -> bool:
     global is_over
     is_over = over
     return is_over
 
 
-move_to = clear_click()
-move_from = clear_click()
-
-selected = "background-color: #ff7"
-unselected = ""
+# ----------- Styling the selected tile -----------
 
 
-def unselect(tile):
+selected = "background-color: #ff7"  # Define selected style as an html string
+unselected = "" # No style for a tile which is unselected
+
+
+def unselect(tile: dict) -> dict:
+    """Change a provided tile's 'selected class' attribute to be unselected, then return the updated tile"""
     tile["selected class"] = unselected
     print(str(tile))
     return tile
 
 
 def select(tile):
+    """Change a provided tile's 'selected class' attribute to be selected, then return the updated tile"""
     tile["selected class"] = selected
     print(str(tile))
     return tile
 
 
-def react_to(rank, file):
+# ----------- Interpreting a click on the board -----------
+
+
+# create global variables to track the most recently clicked tiles
+move_from = clear_click()
+move_to = clear_click()
+
+
+def react_to(rank: int, file: int) -> str:
+    """Handle input from a specified (rank, file) location and change global game state variables accordingly
+    :returns string with feedback for the player with a summary of their most recent move and its impact"""
+
     global move_to, move_from, board
+
+    # Find the tile at the specified location
     clicked_tile = board[rank][file]
     clicked_tile_is_empty = clicked_tile.get("piece").get("label") == "_"
+
+    # Create return string to be filled in later
     ret_str = ""
 
     if move_from == clicked_tile:  # Same tile clicked: cleared
@@ -125,68 +160,85 @@ def react_to(rank, file):
 
     # Try to move!
     else:
+        # The clicked tile is the tile being moved to
         move_to = clicked_tile
         move_from_rank = move_from.get("rank")
         move_from_file = move_from.get("file")
 
+        # Try this movement to see if it worked
         move.new_move(move_from, move_to, board)
-
         can_move = move.get_most_recent_move_successful()
-        if can_move:  # if move is successful
+
+        if can_move:  # move is successful
+            # If this somehow managed to capture the king, the game is over
+            # Note: this should not happen with checkmate stuff currently in place
             if not clicked_tile_is_empty:
                 is_king = board[rank][file]["piece"]["name"] == "king"
                 if is_king:
                     set_is_over(True)
 
+            # if after a successful move, check if the other team is in checkmate
             hypothetical_board = copy.deepcopy(board)
             hypothetical_board[rank][file]["piece"] = move_from.get("piece")
             hypothetical_board[move_from_rank][move_from_file]["piece"] = pieces.get_empty_piece()
-            # if after a successful move, check if the other team is in checkmate
+
             if is_enemy_in_checkmate(hypothetical_board, get_opposite_color(get_cur_turn())):
                 set_is_over(True)
 
+            # Figure out if this move allows a pawn to be promoted and trigger promotion actions if necessary
             to_promotable_space = (get_cur_turn() == "black" and rank == 1) or (get_cur_turn() == "white" and rank == 8)
 
             if move_from.get("piece").get("name") == "pawn" and to_promotable_space:
-                ret_str = " It must now be promoted."
+                ret_str = " It must now be promoted."  # Will be added after the normal part of the return string
                 set_promotion_happening(True)
 
-                board[move_from_rank][move_from_file] = unselect(move_from)
+                # Select the new location
                 board[rank][file] = select(move_to)
 
+            # Clear and unselect the old location, regardless of promotion actions
             board[rank][file]["piece"] = move_from.get("piece")
             board[move_from_rank][move_from_file]["piece"] = pieces.get_empty_piece()
-
-            if not promotion_happening:
-                board[move_from_rank][move_from_file] = unselect(move_from)
-                move_from = clear_click()
-                switch_turns()
-
-            else:
-                move_from = clear_click()
-                switch_turns()
-
-        else:  # if move is not successful
             board[move_from_rank][move_from_file] = unselect(move_from)
             move_from = clear_click()
 
+            # Switch turns if not promoting
+            if not promotion_happening:
+                switch_turns()
+
+        else:  # if move is not successful
+
+            # Clear whatever move was tried since it did not work
+            board[move_from_rank][move_from_file] = unselect(move_from)
+            move_from = clear_click()
+
+        # Clear whatever move was tried, unless it is still needed for promotion
         if not promotion_happening:
             move_to = clear_click()
 
+        # update the return string
         ret_str = move.get_most_recent_feedback() + ret_str
+
+    # return, used by every branch of the if statement
     return ret_str
 
 
-promotion_happening = False
+# ----------- Promotion -----------
 
 
-def set_promotion_happening(value):
+promotion_happening = False  # create global variable to track if the game is in a state requiring promotion
+
+
+def set_promotion_happening(value: bool) -> bool:
+    """Set to true if entering a promotion state, false if leaving a promotion state"""
     global promotion_happening
     promotion_happening = value
     return promotion_happening
 
 
 def get_promotion_info():
+    """Returns a tuple with promotion information.
+    First element is a bool, true if promotion is happening, otherwise false
+    Second element is an array, populated with promotion options unless promotion is not happening"""
     global promotion_happening
     if promotion_happening:
         promotion_options = copy.deepcopy(pieces.get_promotion_options(get_cur_turn()))
@@ -195,22 +247,28 @@ def get_promotion_info():
     return promotion_happening, promotion_options
 
 
-def promote_to(name):
-    global move_to, move_from
+def promote_to(name: str) -> str:
+    """Promotes piece move_to to a specified type of piece"""
+    global move_to
     pieces.promote_at_tile(move_to, name)
-    ret_str = "Pawn was moved to " + move_to.get('coord') + " was promoted to " + name + "."
+    ret_str = "Pawn was moved to " + move_to.get('coord') + " and was promoted to " + name + "."
 
+    # Stop promotion
     set_promotion_happening(False)
 
+    # Unselect everything and clear out turn data, then switch turns
     board[move_to["rank"]][move_to["file"]] = unselect(move_to)
     move_to = clear_click()
-    move_from = clear_click()
     switch_turns()
 
     return ret_str
 
 
+# ----------- Reset or Initialize -----------
+
+
 def reset_game():
+    """Set all the global variables in game.py to their starting values to start or re-start a game"""
     global move_to, move_from, board
     move_to = clear_click()
     move_from = clear_click()
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from game import *
 
 app = Flask(__name__)
 
-cur_turn = create_turn()
-game_board = create_board()
+
+# Set up the initial game state
 select_message = "Welcome"
-is_over = set_is_over(False)
-can_promote = False
-promote_options = []
 reset_game()
+
+# Set up variables this class will use to update the display
+cur_turn = get_cur_turn()
+game_board = get_game_board()
+is_over = get_is_over()
+can_promote = get_promotion_info()[0]
+promote_options = get_promotion_info()[1]
 
 
 @app.route("/", methods=["GET"])
@@ -21,6 +25,7 @@ def home():
 def respond_to_click():
     global cur_turn, game_board, select_message, is_over, can_promote, promote_options
 
+    # If the 'new game' button was pressed
     if request.form.keys().__contains__("new game"):
         reset_game()
         cur_turn = get_cur_turn()
@@ -28,35 +33,41 @@ def respond_to_click():
         is_over = get_is_over()
         select_message = "New game started."
 
+    # If expecting a promotion button to be clicked
     elif can_promote:
-        if request.form.keys().__contains__("rook"):
+        if request.form.keys().__contains__("rook"):  # Promote to rook
             select_message = promote_to("rook")
-        elif request.form.keys().__contains__("knight"):
+        elif request.form.keys().__contains__("knight"):  # Promote to knight
             select_message = promote_to("knight")
-        elif request.form.keys().__contains__("bishop"):
+        elif request.form.keys().__contains__("bishop"):  # Promote to bishop
             select_message = promote_to("bishop")
-        elif request.form.keys().__contains__("queen"):
+        elif request.form.keys().__contains__("queen"):  # Promote to queen
             select_message = promote_to("queen")
-        else:
+        else:  # A different button was pressed, tell user about mistake
             select_message = "You must promote this pawn to one of the options shown below the board."
 
+    # Otherwise, it must have been a click on the board
     else:
+        # Loop through all (non-label) buttons
         for rank in range(1, 9):
             for file in range(1, 9):
-                cur_coord = pieces.get_coord_str(rank, file)
-                r = request.form.keys().__contains__(cur_coord)
-                if r:
-                    new_select_message = react_to(rank, file)
-                    if not new_select_message == "":
+                cur_coord = pieces.get_coord_str(rank, file)  # Figure out the coords are
+                r = request.form.keys().__contains__(cur_coord)  # Check if they match the coords of the clicked button
+
+                if r:  # This is the button that was clicked
+                    new_select_message = react_to(rank, file)  # Update the game state and get the new message
+
+                    if not new_select_message == "":  # Update the message if a new one was provided
                         select_message = new_select_message
 
-                    is_over = get_is_over()
+                    is_over = get_is_over()  # Check if game is over
+                    break  # Stop searching for buttons
 
-                    break
-        if is_over:
+        if is_over:  # Switch to game over screen and announce the winner
             winner = switch_turns()
             return render_template("winner.html", winner=winner)
 
+    # Update all of the variables to prepare to update the display
     cur_turn = get_cur_turn()
     game_board = get_game_board()
 
@@ -64,7 +75,9 @@ def respond_to_click():
     can_promote = promoting[0]
     promote_options = promoting[1]
 
-    return render_template("index.html", curTurn=cur_turn, board=game_board, feedbackMessage=select_message, can_promote=can_promote, promote_options=promote_options)
+    # Update the display
+    return render_template("index.html", curTurn=cur_turn, board=game_board, feedbackMessage=select_message,
+                           can_promote=can_promote, promote_options=promote_options)
 
 
 if __name__ == '__main__':
